638c6a622a21de443d8a577105c9ba73a1c3a966
KosBelozyorov <KosBelozyorov@gmail.com>
2019-10-02 00:47:25 +0300

    Updates

---
+++ precache-manifest.3d2819f587522b51b239e737a3e3d1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f5833633353cb5df2251437e86dc1e",
+    "url": "/react_films-list-use-raw-redux/index.html"
+  },
+  {
+    "revision": "491eb7a0aa350ae4764b",
+    "url": "/react_films-list-use-raw-redux/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "cef216294eeee3685e9b",
+    "url": "/react_films-list-use-raw-redux/static/css/main.735efa35.chunk.css"
+  },
+  {
+    "revision": "491eb7a0aa350ae4764b",
+    "url": "/react_films-list-use-raw-redux/static/js/2.420f8d26.chunk.js"
+  },
+  {
+    "revision": "cef216294eeee3685e9b",
+    "url": "/react_films-list-use-raw-redux/static/js/main.879aacb0.chunk.js"
+  },
+  {
+    "revision": "7588ac357f07406e5d63",
+    "url": "/react_films-list-use-raw-redux/static/js/runtime~main.1e9a4e9f.js"
+  }
+]);
